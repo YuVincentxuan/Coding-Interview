877a42719963627aad9e8c5de73c9fb6b28f5946
YuVincentxuan <937716182@qq.com>
2019-12-13 14:44:08 +0800

    16合并两个排序的链表

---
+++ Code/16合并两个排序的链表.js
@@ -0,0 +1,33 @@
+/*
+    author: Hush!
+    time:   2019-12-05
+*/
+/*
+    题目描述:
+        比较两个链表的大小，进行合并。
+*/
+/*
+    解题思路:
+*/
+/*function ListNode(x){
+    this.val = x;
+    this.next = null;
+}*/
+function Merge(pHead1, pHead2)
+{
+    // write code here
+    if(pHead1 == null){
+        return pHead2;
+    }else if(pHead2 == null){
+        return pHead1;
+    }
+    var pHead = null;
+    if(pHead1.val < pHead2.val){
+        pHead=pHead1
+        pHead.next = Merge(pHead1.next,pHead2)
+    }else{
+        pHead=pHead2
+        pHead.next = Merge(pHead1,pHead2.next)
+    }
+    return pHead;
+}
